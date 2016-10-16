523e57e6112d1d1624b9620128cf8289b9312e84
Derek Lee <dleedev7@gmail.com>
2016-10-15 20:41:25 -0400

    configured gulp to minimize js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function(){
 	console.log('Gulp is running correctly');
-})
+});
+
+gulp.task('uglify', function(){
+	gulp.src('views/js/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('views/dist'));
+});
