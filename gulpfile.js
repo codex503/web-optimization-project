4d5b9ff436169e564c0c1fc1311eacc29fcc8732
Derek Lee <dleedev7@gmail.com>
2016-10-15 21:04:46 -0400

    adding comments on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
+// testing to make sure gulp is properly configured
 gulp.task('default', function(){
 	console.log('Gulp is running correctly');
 });
-
+// task to minify javascript
 gulp.task('uglify', function(){
 	gulp.src('views/js/**/*.js')
 		.pipe(uglify())
