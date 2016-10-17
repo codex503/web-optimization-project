eff679baf6f58a9acae30e7f0fb7c529f5372feb
Derek Lee <dleedev7@gmail.com>
2016-10-17 15:24:49 -0400

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-// testing to make sure gulp is properly configured
-gulp.task('default', function(){
-	console.log('Gulp is running correctly');
-});
 // task to minify javascript
 gulp.task('uglify', function(){
 	gulp.src('views/js/**/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('views/dist'));
 });
+
+// task for responsive images
+
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+ 
+gulp.task('default', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
